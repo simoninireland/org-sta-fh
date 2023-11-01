bd2b6eed0e3f2c94d3f60e8e57d5e1b2663fd751
Simon Dobson <simon.dobson@computer.org>
2023-11-01 15:45:27 +0000

    Incomplete tests

--- test/test-org.el
+++ test/test-org.el
@@ -1,6 +1,6 @@
 ;;; test-org.el --- Test interface to org documents -*- lexical-binding: t -*-
 
-;; Copyright (C) 2015-2022 Free Software Foundation, Inc.
+;; Copyright (c) 2022--2023 Simon Dobson <simoninireland@gmail.com>
 
 ;; Author: Simon Dobson <simoninireland@gmail.com>
 ;; Maintainer: Simon Dobson <simoninireland@gmail.com>
@@ -34,20 +34,20 @@
 
 (ert-deftest matriculation-numbers ()
   "Test we can recognise tric numbers as student identifiers."
-  (should (org-sta-fh--student-identifier? "123456789"))
-  (should (org-sta-fh--student-identifier? "012345678"))
-  (should (not (org-sta-fh--student-identifier? "1234567")))
-  (should (not (org-sta-fh--student-identifier? "1234567890")))
-  (should (not (org-sta-fh--student-identifier? "1234 567")))
-  (should (not (org-sta-fh--student-identifier? "123456789 ")))
-  (should (not (org-sta-fh--student-identifier? "sd80"))))
+  (should (org-sta-fh--valid-student-identifier? "123456789"))
+  (should (org-sta-fh--valid-student-identifier? "012345678"))
+  (should (not (org-sta-fh--valid-student-identifier? "1234567")))
+  (should (not (org-sta-fh--valid-student-identifier? "1234567890")))
+  (should (not (org-sta-fh--valid-student-identifier? "1234 567")))
+  (should (not (org-sta-fh--valid-student-identifier? "123456789 ")))
+  (should (not (org-sta-fh--valid-student-identifier? "sd80"))))
 
 (ert-deftest differentiate-students-grades ()
   "Test we can differentiate a student identifier from a grade.
 (We don't guarantee that the other way doesn't work.)"
-  (should (org-sta-fh--student-identifier? "123456789"))
-  (should-not (org-sta-fh--student-identifier? "10"))
-  (should-not (org-sta-fh--student-identifier? "10.5")))
+  (should (org-sta-fh--valid-student-identifier? "123456789"))
+  (should-not (org-sta-fh--valid-student-identifier? "10"))
+  (should-not (org-sta-fh--valid-student-identifier? "10.5")))
 
 (ert-deftest valid-grades ()
   "Test we can identify valid grades on the 20-point scale."
@@ -56,7 +56,9 @@
   (should (org-sta-fh--valid-grade? "20"))      ; we allow strings as well
   (should (org-sta-fh--valid-grade? 10))
   (should (org-sta-fh--valid-grade? 10.5))
+  (should (org-sta-fh--valid-grade? "10.5"))
   (should-not (org-sta-fh--valid-grade? 21))
+  (should-not (org-sta-fh--valid-grade? "21"))
   (should-not (org-sta-fh--valid-grade? 20.5))
   (should-not (org-sta-fh--valid-grade? 10.1))
   (should-not (org-sta-fh--valid-grade? 10.51))
@@ -77,3 +79,20 @@
   (let ((sgs (org-sta-fh--parse-headline "123456789 234567890 10.5")))
     (should (and (equal (car sgs) (list "123456789" "234567890"))
 		 (= (cdr sgs) 10.5)))))
+
+(ert-deftest submissions ()
+  "Test we can extract submission file patterns."
+  (should (equal (org-sta-fh--filter-submissions (list "123456789"))
+		 (list "123456789")))
+  (should (equal (org-sta-fh--filter-submissions (list))
+		 nil))
+  (should (equal (org-sta-fh--filter-submissions (list "123456789" "234567890"))
+		 (list "123456789" "234567890")))
+  (should (equal (org-sta-fh--filter-submissions (list "1234567890" "234567890"))
+		 (list "234567890")))
+  (should (equal (org-sta-fh--filter-submissions (list "123456789" "2345678900"))
+		 (list "123456789")))
+  (should (equal (org-sta-fh--filter-submissions (list "123456789.zip" "234567890"))
+		 (list "123456789" "234567890")))
+  (should (equal (org-sta-fh--filter-submissions (list "123456789.zip" "234567890.txt"))
+		 (list "123456789" "234567890"))))
